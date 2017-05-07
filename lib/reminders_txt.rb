b2f12c5488d6cba4974e2fc0a5b2353b825f0594
Your Name <you@example.com>
2017-05-07 21:27:33 +0100

    refactoring: The parsing of the calendar event is now done by the event_nlp gem.

--- lib/reminders_txt.rb
+++ lib/reminders_txt.rb
@@ -4,40 +4,28 @@
 
 
 require 'dynarex'
-require 'app-routes'
+require 'event_nlp'
 require 'digest/md5'
-require 'chronic_cron'
-
-
-module Ordinals
-
-  refine Integer do
-    def ordinal
-      self.to_s + ( (10...20).include?(self) ? 'th' : 
-                    %w{ th st nd rd th th th th th th }[self % 10] )
-    end
-  end
-end
-
 
 
 class RemindersTxt
-  include AppRoutes
-  using Ordinals
+
   
   attr_reader :reminders, :dx
   
   def initialize(raw_s='reminders.txt', now: Time.now)
 
     super()
-    
+
     @now = now    
+
+    
     @filepath = raw_s
     
     if raw_s.lines.length > 1 then
 
       if raw_s.lstrip[0] == '<' then
-        
+
         @filepath = 'reminders.xml'
         @dx = Dynarex.new raw_s
         
@@ -68,12 +56,11 @@ class RemindersTxt
   
   def add(s)
 
-    @params = {}    
-    expressions(@params)
-    @params[:input] = s
-
-    @reminders << find_expression(s) 
-
+    s.strip!
+    r = EventNlp.new(@now, params: {input: s}).parse(s)
+    return if r.nil?
+    
+    @reminders << r
     refresh()        
 
   end
@@ -95,160 +82,25 @@ class RemindersTxt
   
   def to_xml()
     @dx.to_xml pretty: true
-  end
-    
-  protected
-
-  def expressions(params) 
-
-    
-    # some event every 2 weeks
-    # some event every 2 weeks at 6am starting from 14th Jan
-    # some event every 2 weeks at 6am starting from 18th Feb until 28th Oct
-    # some event every 2nd Monday (starting 7th Nov 2016)
-    # some event every 2nd Monday (starting 7th Nov until 3rd Dec)
-
-
-    starting = /(?:\(?\s*starting (\d+\w{2} \w+\s*\w*)(?: until (.*))?\s*\))?/
-    weekday = Date::DAYNAMES.join('|').downcase
-    months = (Date::MONTHNAMES[1..-1] + Date::ABBR_MONTHNAMES[1..-1])
-      .join('|').downcase
-
-
-    get /^(.*)(every \w+ \w+(?: at (\d+am) )?)\s*#{starting}/ do \
-                                   |title, recurring, time, raw_date, end_date|
-      
-      input = params[:input]
-      
-      d = Chronic.parse(raw_date)
-      
-      if recurring =~ /day|week/ then
-                
-        
-        if d < @now then
-          
-          new_date = CronFormat.new(ChronicCron.new(recurring)\
-                                    .to_expression, d).to_time
-          input.gsub!(raw_date, new_date\
-                      .strftime("#{new_date.day.ordinal} %b %Y"))        
-          d = new_date
-          
-        end
-      end
-      
-
-      #puts [0, title, recurring, time, raw_date, end_date].inspect
-      OpenStruct.new input: input, title: title, recurring: recurring, 
-                                                date: d, end_date: end_date
-      
-    end
-    
-    # some meeting 3rd thursday of the month at 7:30pm
-    # some meeting First thursday of the month at 7:30pm
-    get /(.*)\s+(\w+ \w+day of (?:the|every) month at .*)/ do 
-                                                             |title, recurring|
-
-      #puts [1, title, recurring].inspect      
-      OpenStruct.new input: params[:input], title: title, recurring: recurring
-
-    end
-    
-    # hall 2 friday at 11am
-    get /(.*)\s+(#{weekday})\s+at\s+(.*)/i do |title, raw_day, time|
-      
-      d = Chronic.parse(raw_day + ' ' + time)
-      
-      #puts [1.5, title, raw_day].inspect
-      OpenStruct.new input: params[:input], title: title, date: d
-      
-    end
-    
-    # e.g. 21/05/2017 Forum meetup at Roundpeg from 2pm
-    get /^(\d+\/\d+\/\d+)\s+(.*)(?: from|at)\s+(\d+[ap]m)/ do 
-                                                    |raw_date, title, raw_time|
-      
-      d = Chronic.parse(raw_date + ' ' + 
-                        raw_time, :endian_precedence => :little)
-      recurring = nil            
-      
-      #puts [3, title, raw_date].inspect
-      OpenStruct.new input: params[:input], title: title, date: d
-    end        
- 
-    # hall 2 friday at 11am
-    # some important day 24th Mar
-    
-    with_date = "(.*)\\s+(\\d\+\s*(?:st|nd|rd|th)?\\s+(?:#{months}))"
-    alt_pattern = '([^\d]+)\s+(\d+[^\*]+)(\*)?'
-    
-    get /#{with_date}|#{alt_pattern}\s*(\*)$/i do |title, raw_date, annualar|
-
-      d = Chronic.parse(raw_date)
-
-      recurring = nil
-      
-      if annualar then
-        
-        recurring = 'yearly'
-        if d < @now then
-          d = Chronic.parse(raw_date, now: Time.local(@now.year + 1, 1, 1)) 
-        end
-      end
-      
-      #puts [2, title, raw_date].inspect
-      OpenStruct.new input: params[:input], title: title, date: d, 
-                                                        recurring: recurring      
-    end
-    
-    # 27-Mar@1436 some important day
-    get /(\d[^\s]+)\s+([^\*]+)(\*)?/ do |raw_date, title, annualar|
-
-      d = Chronic.parse(raw_date, :endian_precedence => :little)
-      recurring = nil
-      
-      if annualar then
-        
-        recurring = 'yearly'
-        if d < @now then
-          d = Chronic.parse(raw_date, now: Time.local(@now.year + 1, 1, 1)) 
-        end
-      end
-      
-      
-      #puts [3, title, raw_date].inspect
-      OpenStruct.new input: params[:input], title: title, date: d, 
-                                                    recurring: recurring      
-    end    
-    
-
-        
-    
-    # e.g. 04-Aug@12:34
-    get '*' do |s|
-      puts 's: ' + s.inspect
-      'pattern unrecognised'
-    end
-
-
-  end
-  
-  alias find_expression run_route
+  end   
   
   private
   
   def import_txt(s)
 
     @file_contents = s
-    @params = {}
-    expressions(@params)
     buffer = s.lines[2..-1]
 
-    @reminders = buffer.inject([]) do |r, x|  
+    @reminders = buffer.inject([]) do |r, x|
+      
+      x.strip!
+
       if (x.length > 1) then
-        @params[:input] = x.strip
-        rx = find_expression(x) 
-        r << rx
+
+        rx = EventNlp.new(@now, params: {input: x}).parse(x)
+        r << rx if rx
       end
+
       r
     end
     
@@ -268,7 +120,7 @@ class RemindersTxt
     if File.exists? @dxfilepath then
 
       @dx = Dynarex.new @dxfilepath
-      
+
       @reminders.each do |reminder|
         s = reminder.input
         r = @dx.find_by_input s
@@ -317,7 +169,6 @@ class RemindersTxt
     @reminders.each {|x| @dx.create x.to_h}
     @dx.save @dxfilepath
     
-  end
-  
+  end  
   
 end
