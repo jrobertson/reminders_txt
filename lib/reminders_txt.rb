61885bc2c521b86bccdb2422deec37e5faea63e3
James Robertson <james@jamesrobertson.me.uk>
2022-02-02 14:43:06 +0000

    minor feature: Implemented public method *this_month* which returns all event reminders for the month ahead.

--- lib/reminders_txt.rb
+++ lib/reminders_txt.rb
@@ -10,7 +10,7 @@ require 'human_speakable'
 
 
 class RemindersTxtException < Exception
-  
+
 
 end
 
@@ -18,137 +18,145 @@ end
 class RemindersTxt
   using ColouredText
   include RXFHelperModule
-  
+
   attr_reader :reminders, :dx
-  
+
   def initialize(raw_s='reminders.txt', now: Time.now, debug: false)
 
     super()
 
     @now, @debug = now, debug
-    
+
     puts ('@now: ' + @now.inspect).debug if @debug
 
-    
+
     @filepath = raw_s
-    
+
     if raw_s.lines.length > 1 then
 
       if raw_s.lstrip[0] == '<' then
 
         @filepath = 'reminders.xml'
         @dx = Dynarex.new raw_s
-        
+
       else
 
         @filepath = File.join(Dir.pwd, 'reminders.txt')
-        @dxfilepath = @filepath.sub(/.txt$/,'.xml')              
+        @dxfilepath = @filepath.sub(/.txt$/,'.xml')
 
         @dx = Dynarex.new
-        import_txt(raw_s)       
-        
+        import_txt(raw_s)
+
       end
-      
+
     elsif File.extname(@filepath) == '.txt'
 
       s = FileX.read @filepath
       @filename =  File.basename(@filepath)
-      @dxfilepath = @filepath.sub(/.txt$/,'.xml')      
-      
+      @dxfilepath = @filepath.sub(/.txt$/,'.xml')
+
       import_txt(s)
-      
+
     else
-      
+
       @dx = Dynarex.new @filepath
-      
+
     end
   end
-  
+
   def add(s)
 
     s.strip!
     r = EventNlp.new(@now, params: {input: s}).parse(s)
     return if r.nil?
-    
+
     @reminders << r
-    refresh()        
+    refresh()
 
   end
-  
+
   def after(d)
-    
+
     date = d.is_a?(String) ? Chronic.parse(d).to_datetime : d
     @dx.filter {|x| DateTime.parse(x.date) > date}
-    
-  end  
-  
+
+  end
+
   def before(d)
-    
+
     future_date = d.is_a?(String) ? Chronic.parse(d).to_datetime : d
     @dx.filter {|x| DateTime.parse(x.date) < future_date}
-    
+
   end
-  
+
   def find(s)
     @dx.filter {|x| x.title =~ /#{s}/i}
   end
 
   def upcoming(ndays=5, days: ndays, months: nil)
-    
+
     next_date = if months then
       @now.to_datetime >> months.to_i
     else
       ((@now.to_date + days.to_i + 1).to_time - 1).to_datetime
     end
-    
+
     @dx.filter {|x| DateTime.parse(x.date) <= next_date}
   end
-    
+
   def updated?()
     @updated
-  end  
-  
+  end
+
   def today()
     upcoming 0
   end
-  
+
   def tomorrow()
     upcoming days: 1
   end
-  
+
   def this_week()
     upcoming days: 6
   end
-  
+
   alias weekahead this_week
-  
+
+  def this_month()
+    upcoming months: 1
+  end
+
   def this_year()
     upcoming months: 12
   end
-  
+
   def to_s()
 
     filename = File.basename(@filepath).sub(/\.xml$/, '.txt')
     [filename,  '=' * filename.length, '', *@dx.all.map(&:input)].join("\n")
 
   end
-  
+
   def to_xml()
     @dx.to_xml pretty: true
-  end   
-  
+  end
+
   private
-  
+
   def import_txt(s)
 
+    puts 'inside import_txt' if @debug
     @file_contents = s
     buffer = s.lines[2..-1]
 
     @reminders = buffer.inject([]) do |r, x|
-      
+
+      puts 'x: ' + x.inspect if @debug
       x.strip!
 
       if (x.length > 1) then
+
+        puts '@now:' + @now.inspect if @debug
 
         rx = EventNlp.new(@now, params: {input: x}, debug: @debug).parse(x)
         puts ('rx: ' + rx.inspect).debug if @debug
@@ -161,15 +169,15 @@ class RemindersTxt
     @updated = false
 
     refresh()
-    
+
   end
-  
+
   # synchronise with the XML file and remove any expired dates
   #
   def refresh()
 
     puts 'inside refresh()' if @debug
-    
+
     reminders = @reminders.clone
     # if XML file doesn't exist, create it
 
@@ -178,18 +186,18 @@ class RemindersTxt
       @dx = Dynarex.new @dxfilepath
 
       @reminders.each do |reminder|
-        
+
         s = reminder.input
         puts ('refresh() checking s: ' + s).debug if @debug
         r = @dx.find_by_input s
-        
+
         # it is on file and it's not a recurring or annual event?
         # use the date from file if the record exists
 
         if (r and r.recurring.empty? and not s[/\*$/]) then
           DateTime.parse(r.date)
-        else          
-          
+        else
+
           if reminder.date then
             reminder.date.to_datetime
           else
@@ -197,74 +205,74 @@ class RemindersTxt
                 + reminder.inspect
           end
         end
-        
+
       end
-      
+
     else
 
       save_dx()
-            
+
     end
 
     # delete expired non-recurring reminders
     @reminders.reject! do |x|
-      
+
       if @debug then
         puts 'rejects filter: '
         puts '  x.input: ' + x.input.inspect
-        puts '  x.date.to_time: '  + x.date.to_time.inspect 
+        puts '  x.date.to_time: '  + x.date.to_time.inspect
       end
-      
+
       x.date.to_time < @now if not x.recurring
-      
+
     end
-    
+
     @reminders.sort_by!(&:date)
 
     # did the reminders change?
     puts 'self.to_s: ' + self.to_s if @debug
-    
+
     h1 = (Digest::MD5.new << self.to_s).to_s
     h2 = (Digest::MD5.new << @file_contents).to_s
 
     b = h1 != h2
-    
+
     if @debug then
       puts 'reminders: ' + reminders.inspect
       puts '@reminders: ' + @reminders.inspect
     end
-    
+
     if b or @reminders != reminders then
 
-      save_dx()      
-      FileX.write File.join(File.dirname(@filepath), 'reminders.txt'), self.to_s 
+      save_dx()
+      FileX.write File.join(File.dirname(@filepath), 'reminders.txt'), self.to_s
       @updated = true
     else
       puts 'no update'
     end
-    
+
     [:refresh, b]
-        
+
   end
 
   def save_detail()
-    
+
     # fetch the notes file if it exists
     filepath = File.dirname @dxfilepath
-        
+
     notesfile = File.join(filepath, 'reminder_notes.xml')
     return unless File.exists? notesfile
-    
-    dx = Dynarex.new notesfile    
 
-    h = dx.all.inject({}) do |r,x| 
+    dx = Dynarex.new notesfile
+
+    h = dx.all.inject({}) do |r,x|
 
       a = x.info.lines
       tag = a.shift[/\w+/]
       body = a.join.strip
-      
+
       r.merge(tag.to_sym => body)
-      
+
     end
 
     rows = @dx.all.map do |x|
@@ -272,46 +280,46 @@ class RemindersTxt
       hashtag ? x.to_h.merge(info: h[hashtag.to_sym]) : x.to_h
     end
 
-    dx2 = Dynarex.new 'reminders/reminder(input, title, recurring, ' + 
+    dx2 = Dynarex.new 'reminders/reminder(input, title, recurring, ' +
         'date, end_date, venue, info)'
     dx2.import rows
     dx2.save File.join(filepath, 'reminder_details.xml')
 
   end
-  
+
   def save_dx()
-    
+
     @dx = Dynarex.new(
       'reminders/reminder(input, title, recurring, date, end_date, venue)')
     @reminders.each {|x| @dx.create x.to_h}
     @dx.save @dxfilepath
-    
+
     save_detail()
-    
+
   end
-  
-  
+
+
 end
 
 class RemindersTxtVoice < RemindersTxt
   using HumanSpeakable
-  
+
   def weekahead() plain_talk(super) end
   def today()     plain_talk(super) end
   def tomorrow()  plain_talk(super) end
-  
+
   private
-  
+
   def plain_talk(entries)
-    
+
     s = entries.all.map do |x|
       date = DateTime.parse(x.date)
       "you are at %s, %s at %s." % [(x.venue.empty? ? x.title : x.venue), \
                                     date.humanize, date.to_time.humanize]
     end.join(" Then ")
 
-    s.sub!(/^./){|x| x.upcase}    
-        
+    s.sub!(/^./){|x| x.upcase}
+
   end
-  
+
 end
--- reminders_txt.gemspec
+++ reminders_txt.gemspec
@@ -1,16 +1,15 @@
 Gem::Specification.new do |s|
   s.name = 'reminders_txt'
-  s.version = '0.6.0'
+  s.version = '0.7.0'
   s.summary = 'Reads and updates diary reminders from a plain text file'
   s.authors = ['James Robertson']
   s.files = Dir['lib/reminders_txt.rb']
-  s.add_runtime_dependency('dynarex', '~> 1.8', '>=1.8.20')
-  s.add_runtime_dependency('event_nlp', '~> 0.5', '>=0.5.4')
-  s.add_runtime_dependency('chronic_cron', '~> 0.6', '>=0.6.0')
-  s.add_runtime_dependency('human_speakable', '~> 0.1', '>=0.1.3')
+  s.add_runtime_dependency('dynarex', '~> 1.9', '>=1.9.2')
+  s.add_runtime_dependency('event_nlp', '~> 0.6', '>=0.6.0')
+  s.add_runtime_dependency('human_speakable', '~> 0.2', '>=0.2.0')
   s.signing_key = '../privatekeys/reminders_txt.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/reminders_txt'
 end
